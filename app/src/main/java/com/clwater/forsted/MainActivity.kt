7accc94e09d4daf049f556903d3ef1c4e9ea1ee4
clwater <gzb@clwater.top>
2023-03-24 18:06:41 +0800

    camera

--- app/src/main/java/com/clwater/forsted/MainActivity.kt
+++ app/src/main/java/com/clwater/forsted/MainActivity.kt
@@ -3,50 +3,23 @@ package com.clwater.forsted
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.camera.core.CameraSelector
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.clwater.forsted.ui.theme.AndroidFrostedGlassBackgroundLiveTheme
-import com.google.common.util.concurrent.ListenableFuture
 
 class MainActivity : ComponentActivity() {
-    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DefaultPreview()
-        }
-    }
-
-
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        AndroidFrostedGlassBackgroundLiveTheme {
-            // A surface container using the 'background' color from the theme
-            Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                Column {
+            AndroidFrostedGlassBackgroundLiveTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     VideoCaptureScreen()
-//                    SmallTopAppBar(
-//                        modifier = Modifier.background(MaterialTheme.colorScheme.primary),
-//                            title = { Text(text = stringResource(id = R.string.app_name)) },
-//                    )
                 }
             }
         }
     }
 }
-
-
